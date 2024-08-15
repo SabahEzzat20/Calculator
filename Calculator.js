4afd9c040d95ad0799fd5831e74bffc98b3d6c88
SabahHassan692 <103220823+SabahHassan692@users.noreply.github.com>
2024-08-16 02:32:46 +0300

    restructuring the code to be clean and covering some issues

--- Calculator.js
+++ Calculator.js
@@ -1,37 +1,56 @@
 let button = document.querySelectorAll('button');
 let operationContainer = document.querySelector('.operationContainer')
-let specialCharacters = ['%','*','/','+','-']
+let specialCharacters = ['%', '*', '/', '+', '-'];
 button.forEach(btn => {
     btn.addEventListener('click', function (e) {
-
-        if (btn.innerHTML !== "DEL" && btn.innerHTML !== 'AC' && btn.innerHTML !== '=') {
-            operationContainer.innerHTML += btn.innerHTML;            
-        }
-        if (operationContainer.innerHTML === '' && specialCharacters.includes(btn.innerHTML)) {
-            // operationContainer.innerHTML = '';
-            return;
-        }
-        if (btn.innerHTML === '=') {
-            operationContainer.innerHTML.length > 0 ? operationContainer.innerHTML = eval(operationContainer.innerHTML) : alert('please enter operation to calculate!');
-            if (operationContainer.innerHTML === '' && specialCharacters.includes(btn.innerHTML)) {
-                operationContainer.innerHTML = '';
-                return;
-            }
-        }
-        if (btn.innerHTML === 'AC') {
-            operationContainer.innerHTML = '';
-        }
-        if (btn.innerHTML === "DEL") {
-            let operation = operationContainer.innerHTML.split('');
-            operation.pop()
-            operationContainer.innerHTML = operation.join('');
-        }
+        handleButtonClick(btn.innerHTML);
     })
-    if (btn.innerHTML === "DEL") {
-        btn.style.color = 'rgb(230, 57, 57)';
+})
+
+
+const clearOperation = () => {
+    operationContainer.innerHTML = '';
+}
+const deleteLastCharacter = () => {
+    let operation = operationContainer.innerHTML.split('');
+    operation.pop();
+    operationContainer.innerHTML = operation.join('');
+}
+const calculateResult = () => {
+    operationContainer.innerHTML = eval(operationContainer.innerHTML);
+}
+const isEmpty = (input) => {
+    return input === '';
+}
+const isSpecialCharacter = (value) => {
+    return specialCharacters.includes(value);
+}
+const appendToOperation = (value) => {
+    if (isSpecialCharacter(value) && operationContainer.innerHTML === '') {
+        return;
+    } else {
+        if(!checkOperator(value))
+            operationContainer.innerHTML += value;
     }
-    if (isNaN(btn.innerHTML)&&btn.innerHTML!=='.'&&btn.innerHTML!=='DEL') {
-            btn.style.color = 'rgb(67, 112, 236)'
+}
+const checkOperator = (value) => {
+    return (specialCharacters.includes(value) && specialCharacters.includes(operationContainer.innerHTML.slice(-1)))
+}
+const handleButtonClick = (value) => {
+    switch (value) {
+        case "DEL":
+            deleteLastCharacter();
+            break;
+        case "AC":
+            clearOperation();
+            break;
+        case "=":
+            calculateResult();
+            break;
+        default:
+            appendToOperation(value);
+            // break;
     }
-});
+}
+
 
